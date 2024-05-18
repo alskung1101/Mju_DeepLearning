7dd8eb81a556d5bfa458cf6783c3d69cf0e0f7c7
CS Yoon <01csyoon@gmail.com>
2024-05-18 21:44:52 +0900

    add jar

--- src/main/java/Repository/RestaurantRepository.java
+++ src/main/java/Repository/RestaurantRepository.java
@@ -1,4 +1,15 @@
 package Repository;
 
-public class RestaurantRepository {
+import Entity.Restaurant;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
+    @Override
+    List<Restaurant> findAll();
+    Optional<Restaurant> findById(@PathVariable Long id);
+
 }
